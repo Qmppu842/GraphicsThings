94fc8f470cd50808c8b85aa8b4af9af47404758d
Qmppu842 <omppu842@gmail.com>
2019-02-23 19:14:02 +0200

    There may or may not be escaping badly cut Pie.

--- src/graphicsthings/GraphicsThings.java
+++ src/graphicsthings/GraphicsThings.java
@@ -5,6 +5,7 @@
  */
 package graphicsthings;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,6 +15,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
@@ -25,19 +29,37 @@ public class GraphicsThings extends Application {
 
     final int WIDTH = 900;
     final int HEIGHT = 900;
+        int asd = 0;
 
     @Override
     public void start(Stage primaryStage) {
         Canvas wall = new Canvas(WIDTH, HEIGHT);
 
         GraphicsContext gc = wall.getGraphicsContext2D();
-        gc.translate(WIDTH/2, WIDTH/2);
+        gc.translate(WIDTH / 2, HEIGHT / 2);
 
-           
-        gc.fillArc(0-50, 0-50, 100, 100, 0, 90, ArcType.ROUND);
-        
+        Paint paint = Color.AQUA;
+//        gc.setFill(paint);
+//        gc.fillArc(0 - 50, 0 - 50, 100, 100, 0, 90, ArcType.ROUND);
+
+        Arc arc = new Arc(WIDTH / 2, HEIGHT / 2, 200, 200, 0, -90);
+        arc.getOnMouseEntered();
+        arc.setType(ArcType.CHORD);
+        arc.setFill(paint);
+        paint = Color.BLACK;
+        arc.setStroke(paint);
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                arc.setTranslateX(asd);
+                asd++;
+            }
+        };
+        timer.start();
         Pane root = new Pane();
         root.getChildren().add(wall);
+        
+        root.getChildren().add(arc);
 
         Scene scene = new Scene(root, WIDTH, HEIGHT);
 
