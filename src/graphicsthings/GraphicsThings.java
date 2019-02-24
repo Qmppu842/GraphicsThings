09b3a0f078c76add7ba5993499774eb6196b8469
Qmppu842 <omppu842@gmail.com>
2019-02-24 14:05:25 +0200

    Apparently I need to implement collision detection manually since intersect doesn't work with moving bodies for some reason...

--- src/graphicsthings/GraphicsThings.java
+++ src/graphicsthings/GraphicsThings.java
@@ -19,6 +19,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -29,7 +30,7 @@ public class GraphicsThings extends Application {
 
     final int WIDTH = 900;
     final int HEIGHT = 900;
-        int asd = 0;
+    int asd = -100;
 
     @Override
     public void start(Stage primaryStage) {
@@ -42,27 +43,43 @@ public class GraphicsThings extends Application {
 //        gc.setFill(paint);
 //        gc.fillArc(0 - 50, 0 - 50, 100, 100, 0, 90, ArcType.ROUND);
 
-        Arc arc = new Arc(WIDTH / 2, HEIGHT / 2, asd, asd, 0, -90);
+        Arc arc = new Arc(WIDTH / 2, HEIGHT / 2, 100, 100, 90, -180);
         arc.getOnMouseEntered();
-        arc.setType(ArcType.CHORD);
-        arc.setFill(paint);
+        arc.setType(ArcType.ROUND);
+        arc.setFill(null);
         paint = Color.BLACK;
         arc.setStroke(paint);
+
+        paint = Color.BLUE;
+        Circle c = new Circle(WIDTH / 2 - 100, HEIGHT / 2, 10, paint);
+         c.setFill(Color.BLUE);
+
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                arc.setTranslateX(asd);
-                arc.setRadiusX(asd);
-                arc.setRadiusY(asd);
-                arc.setRotate(asd);
+//                arc.setTranslateX(asd);
+//                arc.setRadiusX(asd);
+//                arc.setRadiusY(asd);
+//                arc.setRotate(asd);
                 asd++;
+                c.setTranslateX(asd);
+                if (arc.getBoundsInLocal().intersects(c.getBoundsInLocal())) {
+                    c.setFill(Color.RED);
+                    System.out.println("moii");
+                } else {
+                    c.setFill(Color.BLUE);
+                }
+//                if (c.getBoundsInLocal().getMinX()) {
+
+//                }
             }
         };
+
         timer.start();
         Pane root = new Pane();
         root.getChildren().add(wall);
-        
-        root.getChildren().add(arc);
+
+        root.getChildren().addAll(arc, c);
 
         Scene scene = new Scene(root, WIDTH, HEIGHT);
 
